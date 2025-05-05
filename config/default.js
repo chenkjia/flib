13e4e83f32d080aac99d849a288e4149677492cb
chenkjia <chenkjia@live.com>
2025-05-05 10:24:07 +0800

    抓取小时成功,但是保存文件太大

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
   // MongoDB配置
   mongodb: {
-    uri: 'mongodb://localhost:27017/flib/?maxBsonObjectSize=3221225472',
+    uri: 'mongodb://localhost:27017/flib',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true
